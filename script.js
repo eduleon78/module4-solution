9178f7396c87293d2a986795b72208d05b3e98a3
eduleon78 <66341077+eduleon78@users.noreply.github.com>
2021-07-23 07:10:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 //helloSpeaker.speak();
 //byeSpeaker.speak();
 
-(function (window) {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
   for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
+    if (names[i].charAt(0)==='J'||names[i].charAt(0)==='J') {
+      console.log("Good bye " + names[i]);
     } else {
-      helloSpeaker.speak(names[i]);
+      console.log("Hello " + names[i]);
     }
   }
-
-  window.byeSpeaker = helloSpeaker;
-
-})(window);
 
