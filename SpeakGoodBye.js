95a3959cb859f2ddf1298e46e8194a484ac2d707
eduleon78 <66341077+eduleon78@users.noreply.github.com>
2021-07-23 15:03:45 -0400

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,9 +1,8 @@
 (function (window) {
   var byeSpeaker = {};
-  byeSpeaker.name = "John";
   var speakWord = "Good Bye ";
-  byeSpeaker.speak = function () {
-    console.log(speakWord + byeSpeaker.name);
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
 
   window.byeSpeaker = byeSpeaker;
