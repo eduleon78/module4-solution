74aba8e8425001318ea6745a569a35a522e03dd9
eduleon78 <66341077+eduleon78@users.noreply.github.com>
2021-07-23 15:02:27 -0400

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,9 +1,8 @@
 (function (window) {
   var helloSpeaker = {};
-  helloSpeaker.name = "Yaakov";
   var speakWord = "Hello ";
-  helloSpeaker.speak = function () {
-    console.log(speakWord + helloSpeaker.name);
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
 
   window.helloSpeaker = helloSpeaker;
